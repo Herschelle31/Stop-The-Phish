1afde2239ae6cd7bca7dada1f0e23416263fc405
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2025-06-12 22:06:32 +0530

    Add: [brief description of your change]

--- Engineering Module/background.js
+++ Engineering Module/background.js
@@ -2,6 +2,7 @@ chrome.extension.onRequest.addListener(function(prediction){
     if (prediction == 1){
         alert("Warning: Phishing detected!!");
     }
+    
     else if (prediction == -1){
         alert("No phishing detected");
     }
